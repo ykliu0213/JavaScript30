f374458ac1fab84aa4bfbf405e850161c80c83b1
ykLiu <ken23912081@gmail.com>
2019-05-06 18:38:31 +0800

    upload js_22

--- 22 - Follow Along Link Highlighter/script.js
+++ 22 - Follow Along Link Highlighter/script.js
@@ -1,13 +1,18 @@
 
+// 取得HTML中所有的a元素
 const triggers = document.querySelectorAll('a');
+// 建立一個span來放置highlight效果
 const highlight = document.createElement('span');
 highlight.classList.add('highlight');
+// 將建立的span加到頁面中
 document.body.append(highlight);
 
 function highlightLink() {
+    // 取得this(由a.addEventListener傳入，所以會是該a)的資訊
     const linkCoords = this.getBoundingClientRect();
     console.log(linkCoords);
 
+    // 建立一個coords物件來存放會使用的寬高與位置資訊
     const coords = {
         width: linkCoords.width,
         height: linkCoords.height,
@@ -15,11 +20,12 @@ function highlightLink() {
         left: linkCoords.left + window.scrollX
     }
 
+    // 設定highlight效果的寬高及位置
     highlight.style.width = `${coords.width}px`;
     highlight.style.height = `${coords.height}px`;
     highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
 }
 
-
+// 監聽所有a元素的滑鼠移入，觸發highlightLink
 triggers.forEach( a => a.addEventListener('mousemove', highlightLink));
 
