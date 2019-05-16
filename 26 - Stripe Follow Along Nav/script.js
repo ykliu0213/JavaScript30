ec9427979e100b169c457d24b0672bd696420f5f
ykLiu <ken23912081@gmail.com>
2019-05-16 18:20:43 +0800

    upload js_26

---
+++ 26 - Stripe Follow Along Nav/script.js
@@ -0,0 +1,39 @@
+
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+// 滑鼠移入事件
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout( () => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    };
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
+
+// 滑鼠移出事件
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+}
+
+// 為每個選單加上滑鼠移入/移出事件監聽
+triggers.forEach( trigger => trigger.addEventListener('mouseenter',handleEnter));
+triggers.forEach( trigger => trigger.addEventListener('mouseleave',handleLeave));
+
+
+
