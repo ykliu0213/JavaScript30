f6dee1c79f39abc260db24bb7a5aa024c515807f
ykLiu <ken23912081@gmail.com>
2019-05-16 19:09:01 +0800

    upload js_26

--- 26 - Stripe Follow Along Nav/script.js
+++ 26 - Stripe Follow Along Nav/script.js
@@ -5,21 +5,31 @@ const nav = document.querySelector('.top');
 
 // 滑鼠移入事件
 function handleEnter() {
+    // 在觸發的li上加上trigger-enter的class
     this.classList.add('trigger-enter');
+    // 當移入時，先檢查是否有trigger-enter這個className
+    // 若有的話在150毫秒後新增trigger-enter-active這個class
     setTimeout( () => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    // 在白色滑動背景新增open這個class
     background.classList.add('open');
 
+    // 取得滑入元素底下的dropdown
     const dropdown = this.querySelector('.dropdown');
+    // 取得這個dropdown的定位與大小資訊
     const dropdownCoords = dropdown.getBoundingClientRect();
+    // 取得nav的定位與大小資訊
     const navCoords = nav.getBoundingClientRect();
     
+    // 設定將要給白色滑動背景使用的定位與大小資訊
     const coords = {
         height: dropdownCoords.height,
         width: dropdownCoords.width,
+        // 要減去nav的定位，避免上方區塊增加時造成的錯位
         top: dropdownCoords.top - navCoords.top,
         left: dropdownCoords.left - navCoords.left
     };
 
+    // 設定白色滑動背景的定位與大小
     background.style.setProperty('width', `${coords.width}px`);
     background.style.setProperty('height', `${coords.height}px`);
     background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
